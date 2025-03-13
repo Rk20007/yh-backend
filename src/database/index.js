67e283558e9b077fca4dc99e5df8ec46b5233582
webDevSociapa <webdev@sociapa.com>
2025-03-13 12:38:35 +0530

    fofov

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://zubairyh60:VML1kLyKiJmW7ssX@cluster0.ifr8t.mongodb.net/`, 
+      `mongodb+srv://zubairyh60:VML1kLyKiJmW7ssX@cluster0.ifr8t.mongodb.net/mainDb`, 
       {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
         serverSelectionTimeoutMS: 5000, // ⏳ Increase timeout to 5 seconds
         connectTimeoutMS: 10000, // ⏳ Increase connection timeout to 10 seconds
       }
